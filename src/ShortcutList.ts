d24d3cf8db33d3ad3ea0a50e7427cda7a5b45694
Namheon Baek <namhundred@naver.com>
2023-01-28 00:19:26 +0900

    feat: mac/window version, color, dark mode
    add mac/window key convert
    add shortcut of text, background color
    add shortcut of dark mode

--- src/ShortcutList.ts
+++ src/ShortcutList.ts
@@ -5,6 +5,8 @@ const textInfo = {
     i: '기울인 글씨',
     u: '밑줄 긋기',
     s: '취소선',
+    l: '다크 모드',
+    h: '마지막에 쓴 글자/배경색 적용',
     '1': '제목1 생성',
     '2': '제목2 생성',
     '3': '제목3 생성',
@@ -21,6 +23,8 @@ const textInfo = {
     i: 'to italicize text.',
     u: 'to underline text.',
     s: 'to strikethrough.',
+    l: 'to switch on/off dark mode',
+    h: 'to apply the last used text/highlight color.',
     '1': 'to create an H1 heading.',
     '2': 'to create an H2 heading.',
     '3': 'to create an H3 heading.',
@@ -35,7 +39,8 @@ const textInfo = {
 
 export function createShortcutList() {
   let Shortcut: null | HTMLElement = null;
-  const desc = textInfo[navigator.language === 'ko-KR' ? 'ko-KR' : 'en-US'];
+  const desc = textInfo[navigator.language.includes('ko') ? 'ko-KR' : 'en-US'];
+  const ctrl = navigator.platform.includes('Win') ? 'ctrl' : 'cmd';
   return {
     toggle: () => {
       if (!Shortcut) {
@@ -46,19 +51,21 @@ export function createShortcutList() {
         Shortcut.innerHTML = `
         <div class="shortcut-title">${desc.title}</div>
         <div id="close-button">⨉</div>
-        <div><code>cmd/ctrl</code> + <code>B</code> ${desc.b}</div>
-        <div><code>cmd/ctrl</code> + <code>I</code> ${desc.i}</div>
-        <div><code>cmd/ctrl</code> + <code>u</code> ${desc.u}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>s</code> ${desc.s}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>1</code> ${desc['1']}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>2</code> ${desc['2']}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>3</code> ${desc['3']}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>4</code> ${desc['4']}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>5</code> ${desc['5']}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>6</code> ${desc['6']}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>7</code> ${desc['7']}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>8</code> ${desc['8']}</div>
-        <div><code>cmd/ctrl</code> + <code>shift</code> + <code>9</code> ${desc['9']}</div>
+        <div><code>${ctrl}</code> + <code>B</code> ${desc.b}</div>
+        <div><code>${ctrl}</code> + <code>I</code> ${desc.i}</div>
+        <div><code>${ctrl}</code> + <code>u</code> ${desc.u}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>S</code> ${desc.s}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>H</code> ${desc.h}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>L</code> ${desc.l}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>1</code> ${desc['1']}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>2</code> ${desc['2']}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>3</code> ${desc['3']}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>4</code> ${desc['4']}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>5</code> ${desc['5']}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>6</code> ${desc['6']}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>7</code> ${desc['7']}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>8</code> ${desc['8']}</div>
+        <div><code>${ctrl}</code> + <code>shift</code> + <code>9</code> ${desc['9']}</div>
         `;
       } else {
         Shortcut.classList.toggle('close');
