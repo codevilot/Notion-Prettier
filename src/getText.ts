95926ff1cd05cf8a57925e3aef62ff7fe924c932
Namheon Baek <namhundred@naver.com>
2023-08-06 23:58:24 +0900

    update: v0.0.12
    Notion prettier alert on Syntax error

--- src/getText.ts
+++ src/getText.ts
@@ -3,18 +3,24 @@ import parserBabel from 'prettier/parser-babel';
 import parserHtml from 'prettier/parser-HTML';
 import parseryaml from 'prettier/parser-yaml';
 function makePrettier(selection: Selection) {
-  return format(String(selection), {
-    parser: 'babel',
-    plugins: [parserBabel, parserHtml, parseryaml],
-  });
+  try {
+    return format(String(selection), {
+      parser: 'babel',
+      plugins: [parserBabel, parserHtml, parseryaml],
+    });
+  } catch (error) {
+    alert(error);
+    return null;
+  }
 }
 async function copyText(selection: Selection) {
   const textArr = makePrettier(selection);
-  document.execCommand(
-    'insertText',
-    false,
-    textArr.replace(/\n{1,}/gi, '$&\n').trim()
-  );
+  textArr &&
+    document.execCommand(
+      'insertText',
+      false,
+      textArr.replace(/\n{1,}/gi, '$&\n').trim()
+    );
 }
 export function getSelectionText() {
   const selection = getSelection();
